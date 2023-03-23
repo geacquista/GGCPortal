9d877e04ecf96b8a50385c45185f10a246a69989
Gabrielle Acquista <geacquista@wpi.edu>
2023-03-23 17:21:08 -0400

    Build server and add .ebextensions

---
+++ server.compiled.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var _path = _interopRequireDefault(require("path"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+var express = require("express");
+var cors = require("cors");
+var app = express();
+var corsOptions = {
+  origin: "http://localhost:3000"
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express["static"](_path["default"].join(__dirname, 'client', 'build')));
+// simple route
+app.get("/", function (req, res) {
+  res.json({
+    message: "Welcome to GGC Portal Server."
+  });
+});
+
+//ROUTES GO HERE
+require("./app/routes/address.routes.js")(app);
+require("./app/routes/customer.routes.js")(app);
+require("./app/routes/invoice.routes.js")(app);
+require("./app/routes/order.routes.js")(app);
+require("./app/routes/orderline.routes.js")(app);
+require("./app/routes/product.routes.js")(app);
+require("./app/routes/user.routes.js")(app);
+
+// set port, listen for requests
+// 3001 local
+// rds 3306
+var PORT = process.env.PORT || 3001;
+app.listen(PORT, function () {
+  console.log("Server is running on port ".concat(PORT, "."));
+});
