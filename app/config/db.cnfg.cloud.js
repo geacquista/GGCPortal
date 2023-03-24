6fa80643c258289214cdaa8cfea90c78719bda81
Gabrielle Acquista <geacquista@wpi.edu>
2023-03-24 14:57:12 -0400

    reconfig db

--- app/config/db.cnfg.cloud.js
+++ app/config/db.cnfg.cloud.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "ne-ggc-cloud-db-free-tier.cufuu2jymiim.us-east-1.rds.amazonaws.com",
-  USER: "ggc_admin",
+  HOST: "ggc-database-mqp2.cufuu2jymiim.us-east-1.rds.amazonaws.com",
+  USER: "admin",
   PASSWORD: "goodCheeseIsGoat3",
   DB: "GGC_WAREHOUSE_CLOUD",
 };
