c930cbbd0726c631f03aacec9182a8bf81d4228e
Gabrielle Acquista <geacquista@wpi.edu>
2023-03-24 15:52:21 -0400

    reconfig db and add eb environment as origin in server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:3000"
+  origin: "http://goatsportalne-env.eba-jgwabt4g.us-east-1.elasticbeanstalk.com"
 };
 
 app.use(cors(corsOptions));
